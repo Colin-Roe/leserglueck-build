64a37e0fa4301d612959c682d451a77002e16426
Colin Roe <colinkroe@gmail.com>
2020-02-24 11:38:22 +0100

    Add mail.php

--- mail.php
+++ mail.php
@@ -3,74 +3,98 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 require 'vendor/autoload.php';
 
+$err = false;
 
+if (array_key_exists('nameInput', $_POST)) {
+	// Limit length and strip HTML tags
+	$name = substr(strip_tags($_POST['nameInput']),0,255);
+} else {
+	$name = '';
+}
 
-$name = $_POST['nameInput'];
-$visitor_email = $_POST['emailInput'];
-$how = $_POST['wieInput'];
-$subject = $_POST['subjectInput'];
-$message = $_POST['messageInput'];
+// Make sure the email the user provided is valid before trying to use it
+if (array_key_exists('emailInput', $_POST) && PHPMailer::validateAddress($_POST['emailInput'])) {
+	$visitor_email = $_POST['emailInput'];
+}
+else {
+    $err = true;	
+}
+
+if (array_key_exists('subjectInput', $_POST)) {
+	// Limit length and strip HTML tags
+	$subject = substr(strip_tags($_POST['subjectInput']),0,255);
+} else {
+	$err = true;
+}
+
+//Apply some basic validation and filtering to the query
+if (array_key_exists('messageInput', $_POST)) {
+	//Limit length and strip HTML tags
+	$message = substr(strip_tags($_POST['messageInput']), 0, 16384);
+} else {
+	$err = true;
+}
 
 $enquiryData = [
     'fName' => $name,
     'visitorEmail' => $visitor_email,
-    'how' => $how,
     'subject' => $subject,
     'message' => $message
 ];
 
-function sendEmail($enquiryData) {
-    // Instantiation and passing `true` enables exceptions
-    $mail = new PHPMailer(true);
+sendEmail($enquiryData, $err);
 
-    try {
-        //Server settings
-        $mail->SMTPDebug = false;                      // Enable verbose debug output
-        $mail->isSMTP();                                            // Send using SMTP
-        $mail->Host       = 'mx2fc0.netcup.net';                    // Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'dagmar@leserglueck.de';                     // SMTP username
-        $mail->Password   = 'Stuttgart@2020';                               // SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-        $mail->Port       = 587;                                    // TCP port to connect to
+function sendEmail($enquiryData, $err) {
+	if (!$err) {
+		// Instantiation and passing `true` enables exceptions
+		$mail = new PHPMailer(true);
 
-        //Recipients
-        $mail->setFrom('dagmar@leserglueck.de', 'Contact Form');
-        $mail->addAddress('colinkroe@gmail.com', $enquiryData['fName']);    // Add a recipient
-        // $mail->addAddress('ellen@example.com');               // Name is optional
-        // $mail->addReplyTo('info@example.com', 'Information');
-        // $mail->addCC('cc@example.com');
-        // $mail->addBCC('bcc@example.com');
+		try {
+			//Server settings
+			$mail->SMTPDebug = false;                      // Enable verbose debug output
+			$mail->isSMTP();                                            // Send using SMTP
+			$mail->Host       = 'mx2fc0.netcup.net';                    // Set the SMTP server to send through
+			$mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+			$mail->Username   = 'dagmar@leserglueck.de';                     // SMTP username
+			$mail->Password   = 'Stuttgart@2020';                               // SMTP password
+			$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+			$mail->Port       = 587;                                    // TCP port to connect to
 
-        // Attachments
-        // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-        // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+			//Recipients
+			$mail->setFrom('dagmar@leserglueck.de', 'Contact Form');
+			$mail->addAddress('dagmar@leserglueck.de', 'Dagmar Mehling');    // Add a recipient
+			// $mail->addAddress('ellen@example.com');               // Name is optional
+			// $mail->addReplyTo('info@example.com', 'Information');
+			// $mail->addCC('cc@example.com');
+			// $mail->addBCC('bcc@example.com');
 
-        // Content
-        $mail->isHTML(true);                                  // Set email format to HTML
-        $mail->Subject = $enquiryData['subject'];
+			// Attachments
+			// $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+			// $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 
-        $mailContent = "<h2>Contact form submission</h2>
-            <p>Name: " . $enquiryData['fName'] . "</p>" . 
-            "<p>Message: " . $enquiryData['message'] . "</p>" . 
-            "<p>Email: " . $enquiryData['visitorEmail'] . "</p>" .
-            "<p>Wie hast du mich gefunden? " . $enquiryData['how'] . "</p>";
+			// Content
+			$mail->isHTML(true);                                  // Set email format to HTML
+			$mail->Subject = $enquiryData['subject'];
 
-        $mail->Body = $mailContent;
-        $mail->AltBody = strip_tags($mailContent);
+			$mailContent = "<h2>Contact Form Submission</h2>
+				<p>Name: " . $enquiryData['fName'] . "</p>" . 
+				"<p>Email: " . $enquiryData['visitorEmail'] . "</p>" .
+				"<p>Message: " . $enquiryData['message'] . "</p>";
 
-        // Send the message, check for errors
-        if ($mail->send()) {
-            print 'Message has been sent';
-        }
-        else {
-            print 'Message could not be sent. Mailer Error: '. $mail->ErrorInfo;
-        }
-    } catch (Exception $e) {
-        print "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-    }
+			$mail->Body = $mailContent;
+			$mail->AltBody = strip_tags($mailContent);
+
+			// Send the message, check for errors
+			if ($mail->send()) {
+				print 'Message has been sent';
+			}
+			else {
+				print 'Message could not be sent. Mailer Error: '. $mail->ErrorInfo;
+			}
+		} catch (Exception $e) {
+			print "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+		}
+	}
 }
-
-sendEmail($enquiryData);
 
 ?>
